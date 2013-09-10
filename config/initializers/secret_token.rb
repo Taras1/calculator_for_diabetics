8b5087dc337422e36cf058d37c8c1302a124fb3a
Taras <smayzer@bigmir.net>
2013-09-10 16:29:10 +0300

    Sart project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalculatorForDiabetics::Application.config.secret_key_base = 'f8e8aea1a9a6210512afd4a9a49e5497010851accfb642ccabbc6ff8b4ae1ae53cb8925529163eb7f82bb426d17f16fbf2115344aceb010f1bc4486096ca4161'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CalculatorForDiabetics::Application.config.session_store :cookie_store, key: '_calculator_for_diabetics_session'
